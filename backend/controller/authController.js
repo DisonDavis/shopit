7bdfe2efc183fa9d820a429e294cd4a91e56ff1f
disondavis <disondavis@msn.com>
2022-07-10 22:30:52 +0530

    Added register & login apis

---
+++ backend/controller/authController.js
@@ -0,0 +1,54 @@
+const User = require('../models/user')
+
+const ErrorHandler = require('../utils/errorHandler')
+const catchAsyncError = require('../middlewares/catchAsyncErrors')
+
+//Register  a user => /api/v1/register
+exports.registerUser = catchAsyncError(async (req, res, next) => {
+  const { name, email, password } = req.body
+  const user = await User.create({
+    name,
+    email,
+    password,
+    avatar: {
+      public_id: 'avatars/unr_sample_161118_2054_ynlrg',
+      url: 'https://pickaface.net/gallery/avatar/unr_sample_161118_2054_ynlrg.png',
+    },
+  })
+
+  const token = user.getJwtToken()
+
+  res.status(201).json({
+    success: true,
+    token,
+  })
+})
+
+//Login User => /api/v1/login
+exports.loginUser = catchAsyncError(async (req, res, next) => {
+  const { email, password } = req.body
+  //Check if email and password is entered by user
+  if (!email || !password) {
+    return next(new ErrorHandler('Please enter email & password', 400))
+  }
+
+  //Finding user in database
+  const user = await await User.findOne({ email }).select('+password')
+
+  if (!user) {
+    return next(new ErrorHandler('Invalid Email or Password'), 401)
+  }
+
+  //Check if password is correct or not
+  const isPasswordMatched = await user.comparePassword(password)
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler('Invalid Email or Password'), 401)
+  }
+
+  const token = user.getJwtToken()
+
+  res.status(200).json({
+    success: true,
+    token,
+  })
+})
